b6c85426bad1a5621acdd2fa86b85cdad1140984
root <a5822358@gmail.com>
2025-11-09 17:38:17 +0000

    fix(istio): turn on telemetry and change ingress port

--- level0_dashboard/istio.sh
+++ level0_dashboard/istio.sh
@@ -3,12 +3,17 @@ export ISTIO_ARCH="x86_64"
 export ISTIO_DIR="/tmp/istio-$ISTIO_VERSION" 
 
 echo "--- download istio cli"
+mkdir -p $ISTIO_DIR
+cd /tmp
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION TARGET_ARCH=$ISTIO_ARCH sh -
-export PATH=$PWD/bin:$PATH
+export PATH=$ISTIO_DIR/bin:$PATH
+
+cd $ISTIO_DIR
+echo "--- check istio version ---"
 istioctl version
 
 echo "--- install istio controller plane for default ---"
-istioctl install --set profile=default -y
+istioctl install --set profile=default -y  
 kubectl get pods -n istio-system
 
 echo "--- install plugin ---"
